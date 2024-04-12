4f09cde6ef3b9840b09c75841216276bb6a6f6df
Leah Skinner <leahskinner@catalyst.net.nz>
2024-04-12 13:54:07 +1200

    complete challenge 01_07

---
+++ Ex_Files_Learning_PHP/Exercise_Files/01_07_challenge/start.php
@@ -0,0 +1,8 @@
+<?php 
+$name= "Teresa"; // Heading 1
+$city = "NYC";
+$movie = "Star Wars"; //Italics
+$friend = "George"; //Bold
+$candy = "Sour Patch Kids";
+
+echo "<p> $name is going to $city with their friend <b>$friend</b> to see <em>$movie</em>.</p><p>They're going to buy their favourite candy $candy.</p>";
